91bd0e8467c630b29119ddda3a83fd61c3347912
encorem <zhang_m214@163.com>
2018-08-14 23:54:47 +0800

    fix bug

--- 密码生成器/genpass.sh
+++ 密码生成器/genpass.sh
@@ -2,10 +2,9 @@
 
 i=1;while true
 do
-
     pw=`head -c 700 /dev/urandom | tr -dc a-zA-Z0-9'[:punct:]' \
        | sed -r 's/[^0-9a-zA-Z><+\-\{\}\:.&;]//g' | sed 's/\\\\//g' \
-       | cut -c $1-$((i+12))` 
+       | cut -c $i-$((i+12))` 
  #   echo r:$pw
    echo $pw | \
         grep -E '[0-9]+' \
@@ -15,6 +14,6 @@ do
    if [ $? -eq 0 ];then
         break
    fi
-
+   let i++
 done
 echo $pw
